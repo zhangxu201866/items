2fe347c17d43ff63946246dc2980aa2234bfad4a
luoxixi <821335797@qq.com>
2018-09-23 18:18:13 +0800

    luo

--- js/js.js
+++ js/js.js
@@ -16,7 +16,7 @@ let quest = new Promise((resolve, reject) => {
     xhr.send()
 });
 hovers('show1',"随拍");
-hovers('show2',"竞技游戏");
+// hovers('show2',"竞技游戏");
 function hovers(obj,tab1) {
     let Odiv = document.getElementById(obj);
     var _slef = Odiv.getElementsByTagName('a');
@@ -26,16 +26,16 @@ function hovers(obj,tab1) {
     function firstlod(data) {
         let str = ``,
             strplsh = '';
-        strplsh += `<a href="${data.items[0].channel.url}"><dl><dt><img src="${data.items[0].channel.avatar}"><p>${data.items[0].channel.name}</p></dt><dd><p>${data.items[0].channel.status}</p><span class="view">${Math.floor(data.items[0].viewers)}万</span></dd></dl></a>`;
+        strplsh += `<a href="${data.items[0].channel.url}"><dl><dt><img src="${data.items[0].channel.avatar}"><p>${data.items[0].channel.name}</p></dt><dd><p>${data.items[0].channel.status}</p><span class="view">${(data.items[0].viewers/10000).toFixed(2)}万</span></dd></dl></a>`;
         for (let i = 1; i < 6; i++) {
             let cur = data.items[i];
             // console.log(cur);
-            str += `<li><a href="${cur.channel.url}" target='_blank'><p class="picbox"><img src="${cur.channel.avatar}" alt=""><span class="img-up-t">${cur.channel.name}</span></p><div class="item-info"><dl><dt>${cur.channel.status}</dt><dd><span class="left">${cur.viewers}万</span><span class="right">${cur.game[0].name}</span></dd></dl></div></a></li>`;
+            str += `<li><a href="${cur.channel.url}" target='_blank'><p class="picbox"><img src="${cur.channel.avatar}" alt=""><span class="img-up-t">${cur.channel.name}</span></p><div class="item-info"><dl><dt>${cur.channel.status}</dt><dd><span class="left">${(cur.viewers/10000).toFixed(2)}万</span><span class="right">${cur.game[0].name}</span></dd></dl></div></a></li>`;
         }
         giripush.innerHTML = strplsh;
         giri.innerHTML = str;
-        game.innerHTML = str;
-        gamepush.innerHTML = strplsh
+        // game.innerHTML = str;
+        // gamepush.innerHTML = strplsh
     };
 
     quest.then((data) => {
@@ -76,16 +76,14 @@ function hovers(obj,tab1) {
                 quest.then((data) => {
                     let str = ``,
                         strplsh = '';
-                    strplsh += `<a href="${data.items[6].channel.url}"><dl><dt><img src="${data.items[6].channel.avatar}"><p>${data.items[6].channel.name}</p></dt><dd><p>${data.items[6].channel.status}</p><span class="view">${Math.floor(data.items[6].viewers)}万</span></dd></dl></a>`;
+                    strplsh += `<a href="${data.items[6].channel.url}"><dl><dt><img src="${data.items[6].channel.avatar}"><p>${data.items[6].channel.name}</p></dt><dd><p>${data.items[6].channel.status}</p><span class="view">${(data.items[6].viewers/10000).toFixed(2)}万</span></dd></dl></a>`;
                     for (let i = 5; i < 10; i++) {
                         let cur = data.items[i];
                         // console.log(cur);
-                        str += `<li><a href="${cur.channel.url}" target='_blank'><p class="picbox"><img src="${cur.channel.avatar}" alt=""><span class="img-up-t">${cur.channel.name}</span></p><div class="item-info"><dl><dt>${cur.channel.status}</dt><dd><span class="left">${cur.viewers}万</span><span class="right">${cur.game[0].name}</span></dd></dl></div></a></li>`;
+                        str += `<li><a href="${cur.channel.url}" target='_blank'><p class="picbox"><img src="${cur.channel.avatar}" alt=""><span class="img-up-t">${cur.channel.name}</span></p><div class="item-info"><dl><dt>${cur.channel.status}</dt><dd><span class="left">${(cur.viewers/10000).toFixed(2)}万</span><span class="right">${cur.game[0].name}</span></dd></dl></div></a></li>`;
                     }
                     giripush.innerHTML = strplsh;
                     giri.innerHTML = str;
-                    game.innerHTML = str;
-                    gamepush.innerHTML = strplsh
                 })
             }
         }
